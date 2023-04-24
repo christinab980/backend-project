5ecee9e4dc469efd9fb7ed70232ac4355c4d2720
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-04-24 13:07:56 -0700

    redoing express render and adding files'

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,35 @@
 require('dotenv').config();
+const es6Renderer = require("express-es6-template-engine");
+const { setMainView } = require('./utils');
 const express = require("express");
+const navs = require('./data/navs.json');
 
 const server = express();
+server.engine('html', es6Renderer);
+server.set('views', 'views');
+server.set('view engine', 'html')
 
 const PORT = process.env.PORT || 8080;
 
-server.get('/heartbeat', (req, res) => {
-    console.log('heartbeat', req.heartbeat);
-    res.json({message: 'You are at the /heartbeat'})
+server.get('/', (req, res) => {
+   res.render('index', {
+    locals: { navs },
+    partials: setMainView('landing')
+   });
 });
+
+server.get('/login', (req, res) => {
+    res.render('index', {
+     locals: { navs },
+     partials: setMainView('login')
+    });
+ });
+
+ server.get('/heartbeat', (req, res) => {
+    res.json ({
+    "is": "working",
+    });
+ });
 
 server.listen(PORT, () =>
     console.log(`This server is running at POST ${PORT}`)    
