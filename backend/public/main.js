ef151f14fe75155051269aeac2a0511b9d3206ef
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-05-09 16:40:59 -0700

    updated modal

--- backend/public/main.js
+++ backend/public/main.js
@@ -16,17 +16,13 @@ const landingComics = document.getElementById("popular-comics");
 //comic endpoint global values
 const comicPageFeature = document.getElementById('comics-page-feature');
 const comicPageImg = document.getElementById('comic-page-container');
-const comicPageModal = document.getElementById('comic-page-modal')
-const modal = document.querySelector(".modal-section");
-const overlay = document.querySelector(".overlay");
-const closeModalBtn = document.querySelector(".btn-close");
-const comicBookBtn = document.getElementById('comic-feature-img');
+const comicPageModal = document.getElementById('comic-page-modal');
+const closeModalBtn = document.getElementById("btn-close");
 
 //characher endpoint global values
 const spotlightCharacters = document.getElementById('spotlight-character');
 
-
-
+//api keys and private info -- need to put this is a config file
 const publicKey = '9c2c83f57023818abe5c3258493fb406';
 const privateKey = 'bd3e13633fefcb16b1f1d283c66344f1dbddc5ef';
 const timeStamp = 1683240244972;
@@ -382,6 +378,8 @@ async function renderComicsFeaturePage(jsonData) {
   comicPageFeature.append(divResults)
 
   for(let i = 0; i < 15; i ++) {
+   
+
     const div1 = document.createElement('div')
     div1.className = "comic-page-container"
     div1.id = "comic-page-container"
@@ -394,7 +392,8 @@ async function renderComicsFeaturePage(jsonData) {
     const imgPath = jsonData.data.results[i].thumbnail['path']
     const extension = jsonData.data.results[i].thumbnail['extension']
     img.src = imgPath + "." + extension
-    img.id = "comic-feature-img"
+    const comicId = jsonData.data.results[i].id
+    img.id = `comic-feature-img-${comicId}`
     div2.append(img);
 
     const comicName = document.createElement('div')
@@ -403,14 +402,48 @@ async function renderComicsFeaturePage(jsonData) {
     div2.append(comicName)
 
     divResults.append(div1)
-  }
 
+    const comicBookBtn = document.getElementById(`comic-feature-img-${comicId}`);
+    
+    function renderComic() {
+      const renderedComicName = document.getElementById('modal-name')
+      renderedComicName.innerHTML = `${jsonData.data.results[i].title}`
+
+      const renderedComicImg = document.getElementById('modal-img')
+      renderedComicImg.src = `${imgPath + "." + extension}`
+
+      const renderedComicPublishDate = document.getElementById('modal-publish-info')
+      renderedComicPublishDate.innerHTML = `<h3> Published: </h3> ${moment(jsonData.data.results[i].dates[0].date).format("LL")}`
+
+      const modalWriter = document.getElementById('modal-writer')
+      modalWriter.innerHTML = `<h3> Writer: </h3> <p> ${jsonData.data.results[i].creators.items.find((creator) => creator.role === 'writer').name} </p> `
+      
+      const modalCoverArtist = document.getElementById('modal-cover-artist')
+      modalCoverArtist.innerHTML = `<h3> Cover Artist: </h3> <p> ${(jsonData.data.results[i].creators.items.find((creator) => creator.role === "colorist (cover)") !== undefined) ? jsonData.data.results[i].creators.items.find((creator) => creator.role === "colorist (cover)").name : jsonData.data.results[i].creators.items.find((creator) => creator.role === "penciller (cover)").name}</p>`
+      
+      const modalDescription = document.getElementById('modal-description')
+      modalDescription.innerHTML = `<h3> Description: </h3> <p> ${jsonData.data.results[i].description} </p>`
+
+      const modalPriceInfo = document.getElementById('modal-price-info')
+      modalPriceInfo.innerHTML = `<h3> Price: </h3> <p> $${jsonData.data.results[i].prices[0].price} </p>`
+
+      const button1 = document.getElementById('modal-buy-now-button')
+      console.log(button1)
+      button1.innerHTML = `<a href= ${jsonData.data.results[i].urls[1].url} target="_blank"> Buy Now </a>`
+    }
+    
+    comicBookBtn.addEventListener('click', () => {
+      renderComic();
+      openModal();
+    })
+  }
 }
 
 
 async function comicModal(jsonData) {
   const sectionModal = document.createElement('section');
   sectionModal.className = "modal-section hidden"
+  sectionModal.id = "modal-section"
   comicPageModal.append(sectionModal)
 
   const div = document.createElement('div')
@@ -418,91 +451,80 @@ async function comicModal(jsonData) {
   sectionModal.append(div)
 
   const closeButton = document.createElement('button')
+  closeButton.id = 'btn-close'
   closeButton.className = 'btn-close'
   closeButton.textContent = 'x'
+  closeButton.addEventListener('click', () => {
+    closeModal()
+  })
   div.append(closeButton)
 
   const div1 = document.createElement('div')
   div1.className = 'modal-content'
   sectionModal.append(div1)
 
+  const imgContainer = document.createElement('div')
+  imgContainer.className = 'modal-img-container'
+  div1.append(imgContainer)
+
   const comicImg = document.createElement('img')
   comicImg.className = "modal-image"
-  const imgPath = jsonData.data.results[0].thumbnail['path']
-  const extension = jsonData.data.results[0].thumbnail['extension']
-  comicImg.src = imgPath + "." + extension
-  div1.append(comicImg)
+  comicImg.id = "modal-img"
+  imgContainer.append(comicImg)
 
   const comicContainer = document.createElement('div')
   comicContainer.className = "modal-description-container"
   div1.append(comicContainer)
 
   const comicTitle = document.createElement('div')
+  comicTitle.id = "modal-name"
   comicTitle.className = "modal-description"
-  comicTitle.innerHTML = `<p> ${jsonData.data.results[0].title} </p>`
   comicContainer.append(comicTitle)
-  
 
   const div2 = document.createElement('div')
+  div2.id = "modal-publish-info"
   div2.className = 'modal-publish-info'
-  const h3 = document.createElement('h3')
-  const publishedDate = moment(jsonData.data.results[0].dates[0].date).format("LL")
-  h3.textContent = "Published:"
-  div2.append(h3)
-  div2.append(publishedDate)
   comicContainer.append(div2)
 
   const div3 = document.createElement('div')
-  const writerdiv = document.createElement('div')
-  const writerTitle = document.createElement('h3')
-  writerTitle.textContent = "Writer:"
-  writerdiv.innerHTML = ` <p> ${jsonData.data.results[0].creators.items.find((creator) => creator.role === 'writer').name} </p>`
-  div3.append(writerTitle)
-  div3.append(writerdiv)
+  div3.id = 'modal-writer'
   comicContainer.append(div3)
 
   const div4 = document.createElement('div')
-  const artistDiv = document.createElement('div')
-  const artistTitle = document.createElement('h3')
-  artistTitle.textContent = "Cover Artist:"
-  artistDiv.innerHTML = ` <p> ${jsonData.data.results[0].creators.items.find((creator) => creator.role === "colorist (cover)").name} </p>`
-  div4.append(artistTitle)
-  div4.append(artistDiv)
+  div4.id = 'modal-cover-artist'
   comicContainer.append(div4)
 
   const div5 = document.createElement('div')
-  const descriptionDiv = document.createElement('div')
-  const descriptionTitle = document.createElement('h3')
-  descriptionTitle.textContent = "Description:"
-  descriptionDiv.innerHTML = `<p> ${jsonData.data.results[0].description} </p>`
-  div5.append(descriptionTitle)
-  div5.append(descriptionDiv)
+  div5.id ="modal-description"
   comicContainer.append(div5)
 
   const div6 = document.createElement('div')
+  div6.id = "modal-price-info"
   div6.className = 'modal-price-info'
-  const priceDiv = document.createElement('div')
-  const priceTitle = document.createElement('h3')
-  const buyNowButton = document.createElement('button')
-  buyNowButton.textContent = "Buy Now"
-  buyNowButton.className = 'modal-buy-now-button'
-  buyNowButton.id = 'modal-buy-now-button'
-  priceTitle.textContent = "Price:"
-  priceDiv.innerHTML = `<p> $${jsonData.data.results[0].prices[0].price} </p>`
-  div6.append(priceTitle)
-  div6.append(priceDiv)
-  div6.append(buyNowButton)
   comicContainer.append(div6)  
 
+  const buyNowButton = document.createElement('button')
+  buyNowButton.id = 'modal-buy-now-button'
+  buyNowButton.className = 'modal-buy-now-button'
+  comicContainer.append(buyNowButton)
+ 
 } 
 
-function openModal() {
-   modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
+function closeModal() {
+  const modal = document.getElementById("comic-page-modal");
+  const overlay = document.getElementById("overlay");
+
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 }
 
-// comicBookBtn.addEventListener('click', openModal)
+function openModal() {
+  const modal = document.getElementById("comic-page-modal");
+  const overlay = document.getElementById("overlay");
 
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
 
 getComicsPage();
 comicPageFeatureCharacter();
